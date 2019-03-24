de5a95fc7791bfa9880d86619bdf23b7c714fabb
Marcin Sirocki <marcinsirocki@gmail.com>
2019-03-24 15:23:08 +0100

    simple suspense

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,8 +1,40 @@
 import * as React from "react";
-import {FunctionComponent} from "react";
-import {getAllCountries$, IAllCountriesResponse} from "../api/restcountries";
-import {RouteComponentProps} from "@reach/router";
-import {StateStatus, useFetch} from "../useFetch";
+import { FunctionComponent, Fragment, useState, useEffect } from "react";
+import { getAllCountries$, IAllCountriesResponse } from "../api/restcountries";
+import { RouteComponentProps } from "@reach/router";
+import { StateStatus, useFetch } from "../useFetch";
+
+interface IProps {
+  status: StateStatus;
+  maxDuration?: number;
+  children: () => JSX.Element;
+  fallback: JSX.Element;
+}
+
+const SimpleSuspense: FunctionComponent<IProps> = ({
+  status,
+  fallback,
+  children,
+  maxDuration = 0
+}) => {
+  const [showFallback, setShowFallback] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowFallback(true);
+    }, maxDuration);
+  }, []);
+
+  if (status === StateStatus.Success) {
+    return <Fragment>{children()}</Fragment>;
+  }
+
+  if (showFallback) {
+    return fallback;
+  }
+
+  return null;
+};
 
 export const Countries: FunctionComponent<RouteComponentProps> = () => {
   const { status, data } = useFetch<IAllCountriesResponse[]>(
@@ -10,19 +42,23 @@ export const Countries: FunctionComponent<RouteComponentProps> = () => {
     []
   );
 
-  if (status === StateStatus.In_progress) {
-    return <strong>Loading...</strong>;
+  if (status === StateStatus.Failed) {
+    return <strong>ERROR!</strong>;
   }
 
-  if(status === StateStatus.Success) {
-    return (
-      <ul>
-        {data!.map(country => (
-          <li key={country.name}>{country.name}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  return <strong>ERROR!</strong>;
+  return (
+    <SimpleSuspense
+      fallback={<strong>Loading...</strong>}
+      status={status}
+      maxDuration={100}
+    >
+      {() => (
+        <ul>
+          {data!.map(country => (
+            <li key={country.name}>{country.name}</li>
+          ))}
+        </ul>
+      )}
+    </SimpleSuspense>
+  );
 };
